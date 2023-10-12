17cf5813d71e606681cc7f547e0acb973d82a076
OneSingleTopic <maxime.valin@gmail.com>
2023-10-12 18:38:28 +0200

    clean the project to be ready for interviews

--- src/main/java/org/onesingletopic/interview/Main.java
+++ src/main/java/org/onesingletopic/interview/Main.java
@@ -1,20 +1,20 @@
 package org.onesingletopic.interview;
 
+
 import org.onesingletopic.interview.utils.InterviewService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("New Assignment using dependency injection with spring !");
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)){
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
             InterviewService interviewService = context.getBean(InterviewService.class);
 
-            for (Integer i : interviewService.dummy_function(10)){
+            for (Integer i : interviewService.dummy_function(10)) {
                 System.out.println("i = " + i);
             }
         }
+
+
     }
 }
